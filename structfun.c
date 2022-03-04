41df9f208a0fa4b543e85d15f17d1c70bfcdd452
sangeeth <sangeeth@gmail.com>
2022-03-04 18:09:43 +0530

    structures using funtions

---
+++ structfun.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+struct student func1(struct student ); 
+struct student
+{
+    char name[30];    
+	int roll;
+};
+int main()
+{
+    struct student s1={"sangeeth",411};
+    func1(s1); 
+    return 0;
+}
+
+struct student func1(struct student s)
+{
+    printf("Name: %s\nRoll no: %d\n",s.name,s.roll);
+}
+
+
